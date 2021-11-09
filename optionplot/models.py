7737a93cf8b22cd79327e7312e0c034d1602f9ff
Anthony Tam <40433247+tlapfai@users.noreply.github.com>
2021-11-09 17:39:04 +0800

    Update models.py

--- optionplot/models.py
+++ optionplot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class IRCurve(models.Model):
+    pass
+
+class EuropeanOption(models.Model):
+    local_curve = models.ForeignKey("IRCurve", on_delete=models.DO_NOTHING, related_name="options_as_r")
+    foreign_curve = models.ForeignKey("IRCurve", on_delete=models.DO_NOTHING, related_name="options_as_q")
+    content = models.TextField(null=True)
